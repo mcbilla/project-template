0ee3813d9e330d7e423e9fee12b9a1c34c35ecca
Libra_mcb <mcb9141018@outlook.com>
2022-07-27 00:48:12 +0800

    init

---
+++ src/test/java/com/mcb/project/template/ProjectTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcb.project.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
